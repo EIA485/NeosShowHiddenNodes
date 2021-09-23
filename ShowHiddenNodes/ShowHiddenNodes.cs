eca644421c68de9615390687e9ed86d0a9a0fe2d
eia485 <eia485git@gmail.com>
2021-09-23 09:46:21 -0700

    Add project files.

---
+++ ShowHiddenNodes/ShowHiddenNodes.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using BaseX;
+using System;
+using CloudX.Shared;
+using FrooxEngine.LogiX;
+namespace ShowHiddenNodes
+{
+    public class ShowHiddenNodes : NeosMod
+    {
+        public override string Name => "ShowHiddenNodes";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosShowHiddenNodes/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.ShowHiddenNodes");
+            harmony.PatchAll();
+        }
+        [HarmonyPatch(typeof(LogixHelper), "IsHidden")]
+        class ShowHiddenNodesPatch
+        {
+            static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                    return false;
+            }
+        }
+    }
+}
